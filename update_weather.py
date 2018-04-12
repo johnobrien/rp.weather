3cb4a69dc4f7e4038d0c5aa2e35ff52956fd826a
John OBrien <john.obrien@gmail.com>
2018-04-12 15:03:30 -0400

    Switched to PapirusTextPos

--- update_weather.py
+++ update_weather.py
@@ -9,7 +9,7 @@ from datetime import date, timedelta, datetime
 LEXINGTON = 42.4430, 71.2290
 
 try:
-    from papirus import PapirusTextPos
+    from papirus import PapirusComposite
 except ModuleNotFoundError:
     ## The papirus module is not installed
     ## so let's use a dummy class for it that
@@ -22,23 +22,27 @@ key = os.getenv("dark_sky_api_key")
 if not key:
     raise Exception("DarkSky API Key not set. Set DarkSky environment variable in the local environment and try again.")
 
-text = PapirusTextPos()
 output = ""
+
+''' Possible day.icon values:
+clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night.'''
 
 try:
     weekday = date.today()
     with forecast(key, *LEXINGTON) as boston:
         for day in boston.daily[0:3]:
             day = dict(day=date.strftime(weekday, '%a'),
-                       sum=day.summary,
+                       icon=day.icon,
                        tempMin=round(day.temperatureMin),
                        tempMax=round(day.temperatureMax)
                        )
-            output += ('{day}: {sum} \n'.format(**day))
+            output += ('{day}: {icon} \n'.format(**day))
             weekday += timedelta(days=1)
-    output += 'Last Updated: {0}'.format(datetime.now())
+    # output += 'Last Updated: {0}'.format(datetime.now())
 
-    text.AddText(output)
+    textNImg = PapirusComposite(False)
+    textNImg.AddImg('/assets/icons/cloud.bmp', Id="Top")
+    textNImg.WriteAll()
 except:
     # Couldn't access Dark Sky, so fail gracefully
     # probably writing to a log.
