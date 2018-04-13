0aa217246660ff8f2e408061859efef14f74a6f8
John OBrien <john.obrien@gmail.com>
2018-04-12 22:35:22 -0400

    Trying my hand at scripting

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-# The script that on boot checks
+ls# The script that on boot checks
 # whether a USB device is attached, and if not,
 # update the git repo, executes update_weather.py,
 # and shuts down the raspberry pi.
@@ -8,11 +8,13 @@
 # The path to this script needs to be added to /etc/rc.local
 
 
-if not(lsusb -v 2>/dev/null|grep -q 'bInterfaceProtocol.*Keyboard' &&)
-    echo "Keyboard not present"
+#if not(lsusb -v 2>/dev/null|grep -q 'bInterfaceProtocol.*Keyboard' &&)
+#    echo "Keyboard not present"
     # git --git-dir=/path/to/kd.weather/dir/on/pi pull
     # python /path/to/update_weather.py/on/pi
     # python /path/to/set_reboot.py/on/pi
     # sudo poweroff
-else
-    echo "Keyboard present"
+#else
+#    echo "Keyboard present"
+
+echo "hello, scripting world"
