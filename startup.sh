2baca8ef8f12c47592b8827f5a98512d3025f3f7
John OBrien <john.obrien@gmail.com>
2018-04-12 22:36:13 -0400

    Trying my hand at scripting

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-ls# The script that on boot checks
+# The script that on boot checks
 # whether a USB device is attached, and if not,
 # update the git repo, executes update_weather.py,
 # and shuts down the raspberry pi.
