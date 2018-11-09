29238da9e1d347003b0a8a4dad646ee8b1129faf
John <john.obrien@gmail.com>
2018-11-09 09:34:21 -0500

    Set to run at the top of every hour. Maybe? Hopefully?

--- run_schedule.py
+++ run_schedule.py
@@ -4,7 +4,23 @@ import threading
 
 from update_weather import update_weather
 
-schedule.every().hour.do(update_weather)
+schedule.every().day.at("6:00").do(update_weather)
+schedule.every().day.at("7:00").do(update_weather)
+schedule.every().day.at("8:00").do(update_weather)
+schedule.every().day.at("9:00").do(update_weather)
+schedule.every().day.at("10:00").do(update_weather)
+schedule.every().day.at("11:00").do(update_weather)
+schedule.every().day.at("12:00").do(update_weather)
+schedule.every().day.at("13:00").do(update_weather)
+schedule.every().day.at("14:00").do(update_weather)
+schedule.every().day.at("15:00").do(update_weather)
+schedule.every().day.at("16:00").do(update_weather)
+schedule.every().day.at("17:00").do(update_weather)
+schedule.every().day.at("18:00").do(update_weather)
+schedule.every().day.at("19:00").do(update_weather)
+schedule.every().day.at("20:00").do(update_weather)
+schedule.every().day.at("21:00").do(update_weather)
+schedule.every().day.at("22:00").do(update_weather)
 
 
 while True:
